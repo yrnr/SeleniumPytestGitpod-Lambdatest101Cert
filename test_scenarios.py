ece4cadd122a5f5280dc4d34b2693b71c1d83d0c
yrnr <iamramuhere@gmail.com>
2022-09-24 14:34:05 +0000

    Let assert raise exception instead of try except

--- test_scenarios.py
+++ test_scenarios.py
@@ -26,62 +26,58 @@ class Test_Scenarios:
     
     home_url = 'https://www.lambdatest.com/selenium-playground'
 
+
+    @pytest.mark.order(1)
     @pytest.mark.timeout(30)
     def test_scenario_1(self):
 
         """ test_scenario_1: simple-form-demo """
-        
-        try:
-            self.driver.get(home_url)
-            self.driver.find_element(By.LINK_TEXT, ps1.LINK_TEXT_SIMPLE_FORM_DEMO).click()
-            assert ps1.simple_form_demo_url in self.driver.current_url
-            self.driver.find_element(By.XPATH, ps1.XPATH_USER_MESSAGE).send_keys(ps1.welcome_text)
-            self.driver.find_element(By.ID, ps1.ID_SHOW_INPUT_BUTTON).click()
-            assert ps1.welcome_text == self.driver.find_element(By.XPATH, ps1.XPATH_WELCOME_MESSAGE).text
-        except Exception as e:
-            print("Test encountered an unexpected error!\n", str(e))    
 
+        self.driver.get(Test_Scenarios.home_url)
+        self.driver.find_element(By.LINK_TEXT, ps1.LINK_TEXT_SIMPLE_FORM_DEMO).click()
+        assert ps1.simple_form_demo_url in self.driver.current_url
+        self.driver.find_element(By.XPATH, ps1.XPATH_USER_MESSAGE).send_keys(ps1.welcome_text)
+        self.driver.find_element(By.ID, ps1.ID_SHOW_INPUT_BUTTON).click()
+        assert ps1.welcome_text == self.driver.find_element(By.XPATH, ps1.XPATH_WELCOME_MESSAGE).text
+
+
+    @pytest.mark.order(2)
     @pytest.mark.timeout(90)
     def test_scenario_2(self):
         
         """ test_scenario_2: Drag & Drop Sliders """
         
-        try:
-            self.driver.get(home_url)
-            self.driver.find_element(By.LINK_TEXT, ps2.LINK_TEXT_DRAG_DROP_SLIDERS).click()
-            ddslider = self.driver.find_element(By.XPATH, ps2.XPATH_DRAG_15_95_SLIDER)
-            for i in range(80):
-                ddslider.send_keys(Keys.ARROW_RIGHT)
-            ddoutput = self.driver.find_element(By.XPATH, ps2.XPATH_RANGE_OUTPUT).text
-            assert int(ddoutput) == 95
-        except Exception as e:
-            print("Test encountered an unexpected error!\n", str(e))
+        self.driver.get(Test_Scenarios.home_url)
+        self.driver.find_element(By.LINK_TEXT, ps2.LINK_TEXT_DRAG_DROP_SLIDERS).click()
+        ddslider = self.driver.find_element(By.XPATH, ps2.XPATH_DRAG_15_95_SLIDER)
+        for i in range(80):
+            ddslider.send_keys(Keys.ARROW_RIGHT)
+        ddoutput = self.driver.find_element(By.XPATH, ps2.XPATH_RANGE_OUTPUT).text
+        assert int(ddoutput) == 95
+
     
+    @pytest.mark.order(3)
     @pytest.mark.timeout(50)
     def test_scenario_3(self):
 
         """ test_scenario_3: Input Form Submit """
 
-        try:
-            self.driver.get(home_url)
-            self.driver.find_element(By.LINK_TEXT, ps3.LINK_TEXT_INPUT_FORM_SUBMIT).click()
-            self.driver.find_element(By.XPATH, ps3.XPATH_BUTTON_SUBMIT).click()
-            validation_msg = self.driver.find_element(By.XPATH, ps3.XPATH_NAME_FIELD).get_property('validationMessage')
-            assert ps3.validation_message in validation_msg
-            self.driver.find_element(By.XPATH, ps3.name[1]).send_keys(ps3.name[0])
-            self.driver.find_element(By.XPATH, ps3.email[1]).send_keys(ps3.email[0])
-            self.driver.find_element(By.XPATH, ps3.password[1]).send_keys(ps3.password[0])
-            self.driver.find_element(By.XPATH, ps3.company[1]).send_keys(ps3.company[0])
-            self.driver.find_element(By.XPATH, ps3.website[1]).send_keys(ps3.website[0])
-            self.driver.find_element(By.XPATH, ps3.city[1]).send_keys(ps3.city[0])
-            self.driver.find_element(By.XPATH, ps3.addr1[1]).send_keys(ps3.addr1[0])
-            self.driver.find_element(By.XPATH, ps3.addr2[1]).send_keys(ps3.addr2[0])
-            self.driver.find_element(By.XPATH, ps3.state[1]).send_keys(ps3.state[0])
-            self.driver.find_element(By.XPATH, ps3.zip_code[1]).send_keys(ps3.zip_code[0])
-            country_select = Select(self.driver.find_element(By.XPATH, ps3.XPATH_COUNTRY_DROPDOWN))
-            country_select.select_by_visible_text(ps3.country)
-            self.driver.find_element(By.XPATH, ps3.XPATH_BUTTON_SUBMIT).click()
-            assert self.driver.find_element(By.XPATH, ps3.success_message[1]).text == ps3.success_message[0]
-        except Exception as e:
-            print("Test encountered an unexpected error!\n", str(e))
-
+        self.driver.get(Test_Scenarios.home_url)
+        self.driver.find_element(By.LINK_TEXT, ps3.LINK_TEXT_INPUT_FORM_SUBMIT).click()
+        self.driver.find_element(By.XPATH, ps3.XPATH_BUTTON_SUBMIT).click()
+        validation_msg = self.driver.find_element(By.XPATH, ps3.XPATH_NAME_FIELD).get_property('validationMessage')
+        assert ps3.validation_message in validation_msg
+        self.driver.find_element(By.XPATH, ps3.name[1]).send_keys(ps3.name[0])
+        self.driver.find_element(By.XPATH, ps3.email[1]).send_keys(ps3.email[0])
+        self.driver.find_element(By.XPATH, ps3.password[1]).send_keys(ps3.password[0])
+        self.driver.find_element(By.XPATH, ps3.company[1]).send_keys(ps3.company[0])
+        self.driver.find_element(By.XPATH, ps3.website[1]).send_keys(ps3.website[0])
+        self.driver.find_element(By.XPATH, ps3.city[1]).send_keys(ps3.city[0])
+        self.driver.find_element(By.XPATH, ps3.addr1[1]).send_keys(ps3.addr1[0])
+        self.driver.find_element(By.XPATH, ps3.addr2[1]).send_keys(ps3.addr2[0])
+        self.driver.find_element(By.XPATH, ps3.state[1]).send_keys(ps3.state[0])
+        self.driver.find_element(By.XPATH, ps3.zip_code[1]).send_keys(ps3.zip_code[0])
+        country_select = Select(self.driver.find_element(By.XPATH, ps3.XPATH_COUNTRY_DROPDOWN))
+        country_select.select_by_visible_text(ps3.country)
+        self.driver.find_element(By.XPATH, ps3.XPATH_BUTTON_SUBMIT).click()
+        assert self.driver.find_element(By.XPATH, ps3.success_message[1]).text == ps3.success_message[0]
